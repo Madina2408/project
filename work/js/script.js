7dadb3819a75ae8098e53a7a81f6a22402158cb4
Madina <madina.zulaeva@mail.ru>
2021-08-13 19:16:33 +0300

    first commit

---
+++ work/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let answers = [];
+
+    answers[0] = prompt('name', '');
+    answers[1] = prompt('sername', '');
+    answers[2] = prompt('year', '');
+    
+
+console.log(typeof(answers));
+
+console.log(typeof("1"));
